428a47329236081e507b9a3a7154186711b47b58
AnhJenda <93340795+AnhJenda@users.noreply.github.com>
2022-05-13 19:14:29 +0700

    update

--- src/Day4/DemoSelectInsertUpdateDelete/DemoInsertUpdateDeleteMySQL.java
+++ src/Day4/DemoSelectInsertUpdateDelete/DemoInsertUpdateDeleteMySQL.java
@@ -16,37 +16,37 @@ public class DemoInsertUpdateDeleteMySQL {
                 Statement stmt = conn.createStatement();
                 ){
             // Demo Insert
-            System.out.println("Demo Insert nhieu ban ghi");
-            String stmMultipleInsert = "insert into category values" + "('10', 'Loai 10')," + "('11', 'Loai 11')";
-            System.out.println("The SQL statement is: " + stmMultipleInsert + "\n");
-            int countMultipleInsert = stmt.executeUpdate(stmMultipleInsert);
-            System.out.println(countMultipleInsert + " ban ghi da insert.\n");
+//            System.out.println("Demo Insert nhieu ban ghi");
+//            String stmMultipleInsert = "insert into category values" + "('10', 'Loai 10')," + "('11', 'Loai 11')";
+//            System.out.println("The SQL statement is: " + stmMultipleInsert + "\n");
+//            int countMultipleInsert = stmt.executeUpdate(stmMultipleInsert);
+//            System.out.println(countMultipleInsert + " ban ghi da insert.\n");
             // Demo Insert 1 phan cua ban ghi
-            System.out.println("Demo Insert 1 phan cua ban ghi");
-            String stmParticalInsert = "insert into book(bookID, bookName, author) values ('1', 'Sach 1', 'Tac gia 1')";
-            int countParticalInserted = stmt.executeUpdate(stmParticalInsert);
-            System.out.println(countParticalInserted + " ban ghi da insert.\n");
+//            System.out.println("Demo Insert 1 phan cua ban ghi");
+//            String stmParticalInsert = "insert into book(bookID, bookName, author) values ('1', 'Sach 1', 'Tac gia 1')";
+//            int countParticalInserted = stmt.executeUpdate(stmParticalInsert);
+//            System.out.println(countParticalInserted + " ban ghi da insert.\n");
             // Demo Scanner Insert
-            System.out.println("Demo Scanner Insert");
-            Scanner in = new Scanner(System.in);
-            System.out.println("Nhap bookId");
-            Integer id = in.nextInt();
-            in.nextLine();
-            System.out.println("Nhap bookName");
-            String bookName = in.nextLine();
-            System.out.println("Nhap author");
-            String author = in.nextLine();
-            String stmScannerInsert = "insert  into book(bookID,bookName,author) values ('" + id + "','" + bookName +"','" + author + "')";
-            int countScannerInserted =  stmt.executeUpdate(stmScannerInsert);
-            System.out.println(countScannerInserted + " ban ghi da insert.\n");
+//            System.out.println("Demo Scanner Insert");
+//            Scanner in = new Scanner(System.in);
+//            System.out.println("Nhap bookId");
+//            Integer id = in.nextInt();
+//            in.nextLine();
+//            System.out.println("Nhap bookName");
+//            String bookName = in.nextLine();
+//            System.out.println("Nhap author");
+//            String author = in.nextLine();
+//            String stmScannerInsert = "insert  into book(bookID,bookName,author) values ('" + id + "','" + bookName +"','" + author + "')";
+//            int countScannerInserted =  stmt.executeUpdate(stmScannerInsert);
+//            System.out.println(countScannerInserted + " ban ghi da insert.\n");
 
             // Demo Delete
-            String stmDelete = "delete from category where catID = 4";
-            int countDeleted = stmt.executeUpdate(stmDelete);
-            System.out.println(countDeleted + " records deleted.\n");
+//            String stmDelete = "delete from category where catID = 10";
+//            int countDeleted = stmt.executeUpdate(stmDelete);
+//            System.out.println(countDeleted + " records deleted.\n");
 
-            // Demo Update
-            String stmUpdate = "update category" + "set catID = 1" +  "where catID = 5";
+//            // Demo Update
+            String stmUpdate = "update category" + " set catID = 1" +  " where catID = 11";
             int countUpdated = stmt.executeUpdate(stmUpdate);
             System.out.println(countUpdated + " records updated.\n");
         }
---
+++ src/Day5/JdbcCommitCatchTest.java
@@ -0,0 +1,33 @@
+package Day5;
+
+/*
+    @author: Dinh Quang Anh
+    Date   : 5/12/2022
+    Project: Java2
+*/
+import java.sql.*;
+public class JdbcCommitCatchTest {
+    public static void main(String[] args) throws SQLException {
+        try (
+                Connection conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:8888/ebookshop", "myuser", "xxxx"); // MySQL
+                Statement stmt = conn.createStatement();
+        ) {
+            try {
+                // Disable auto-commit
+                conn.setAutoCommit(false);
+
+                // Issue two INSERT statements
+                stmt.executeUpdate("insert into books values (4001, 'Paul Chan', 'Mahjong 101', 4.4, 4)");
+                // Duplicate primary key, which triggers a SQLException
+                stmt.executeUpdate("insert into books values (4001, 'Peter Chan', 'Mahjong 102', 4.4, 4)");
+                conn.commit();     // Commit changes only if all statements succeed.
+
+            } catch(SQLException ex) {
+                System.out.println("-- Rolling back changes --");
+                conn.rollback();   // Rollback to the last commit.
+                ex.printStackTrace();
+            }
+        }
+    }
+}
